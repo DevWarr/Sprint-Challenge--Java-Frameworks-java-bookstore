10800e59d0992179458f3005ded0b50940dc2cf7
Devin Warrick <dev.warr@gmail.com>
2019-07-26 13:30:38 -0500

    Updated Authentication. Users can no longer access the POST/PUT/DELETE requests.

--- StartHere/src/main/java/com/lambdaschool/bookstore/config/ResourceServerConfig.java
+++ StartHere/src/main/java/com/lambdaschool/bookstore/config/ResourceServerConfig.java
@@ -38,6 +38,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter
                 .antMatchers("/users/**", "/oauth/revoke-token").authenticated()
                 .antMatchers("/roles/**").hasAnyRole("ADMIN", "USER", "DATA")
                 .antMatchers("/actuator/**").hasAnyRole("ADMIN")
+                .antMatchers("/data/**").hasAnyRole("ADMIN", "DATA")
                 .and().exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
 
         // http.requiresChannel().anyRequest().requiresSecure();
