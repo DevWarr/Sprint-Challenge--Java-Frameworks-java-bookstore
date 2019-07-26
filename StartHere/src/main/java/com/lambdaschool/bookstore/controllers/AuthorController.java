6d85f83e69be9cfddc32bc253162013aa8f2f4ff
Devin Warrick <dev.warr@gmail.com>
2019-07-26 12:12:27 -0500

    findAll() for authors controller works properly.

--- StartHere/src/main/java/com/lambdaschool/bookstore/controllers/AuthorController.java
+++ StartHere/src/main/java/com/lambdaschool/bookstore/controllers/AuthorController.java
@@ -1,9 +1,21 @@
 package com.lambdaschool.bookstore.controllers;
 
+import com.lambdaschool.bookstore.services.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthorController
 {
+    @Autowired
+    AuthorService authorService;
 
+    @GetMapping(value = "/authors", produces = {"application/json"})
+    public ResponseEntity<?> findAllAuthors()
+    {
+        return new ResponseEntity<>(authorService.findAll(), HttpStatus.OK);
+    }
 }
--- StartHere/src/main/java/com/lambdaschool/bookstore/services/AuthorServiceImpl.java
+++ StartHere/src/main/java/com/lambdaschool/bookstore/services/AuthorServiceImpl.java
@@ -1,15 +1,24 @@
 package com.lambdaschool.bookstore.services;
 
 import com.lambdaschool.bookstore.models.Author;
+import com.lambdaschool.bookstore.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service(value = "authorService")
 public class AuthorServiceImpl implements AuthorService
 {
+    @Autowired
+    AuthorRepository authorrepo;
+
     @Override
-    public List<Author> findAll() {
-        return null;
+    public List<Author> findAll()
+    {
+        List<Author> authors = new ArrayList<>();
+        authorrepo.findAll().iterator().forEachRemaining(authors::add);
+        return authors;
     }
 }
